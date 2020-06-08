77c064ff4525ca15fdf50e74399f0ce293ae99e2
Seshagiri Rao Kornepati <seshagirirao1992@gmail.com>
2020-06-07 22:27:23 -0700

    /src/views/Orgs/Org/org.js

--- src/views/Orgs/Org/org.js
+++ src/views/Orgs/Org/org.js
@@ -1,8 +1,7 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 
-import './org.css'
-import { AppContext } from "../../../components/Context/appContext";
-import { makeStyles } from '@material-ui/core/styles';
+//import './org.css'
+import {AppContext} from "../../../components/Context/appContext";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -12,65 +11,59 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import TextField from "@material-ui/core/TextField";
 import {Link} from "react-router-dom";
+import Tooltip from "@material-ui/core/Tooltip";
+import InfoIcon from "@material-ui/icons/Info";
 
-function filterOrgs() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("orgSearch");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
+function filterOrgs(unfiltered, columnName, searchString) {
+    if (searchString === "") return [...unfiltered]
+    return unfiltered.filter((entry) => {
+        return (entry[columnName] && entry[columnName].toUpperCase().indexOf(searchString.toUpperCase()) >= 0)
+    })
 }
 
 const Org = () => {
     const [contextValue] = useContext(AppContext);
+    const [orgList, setOrgList] = useState([]);
+    const [orgListFiltered, setOrgListFiltered] = useState([]);
+
+    // Initialization
+    useEffect(() => {
+        if (contextValue.orgList.length > 0) {
+            let orgs = []
+            contextValue.orgList.forEach(entry => {
+                orgs.push({
+                    id: entry.id,
+                    name: entry.name,
+                    url: entry.url
+                })
+            });
+            setOrgList([...orgs]);
+            setOrgListFiltered([...orgs]);
+        }
+    }, [contextValue]);
+
+    // Network List Pagination
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-    const rows = [];
-    const columns = [
-        { id: 'id', label: 'Org ID', minWidth: 100 },
-        { id: 'name', label: 'Org Name', minWidth: 170 },
-    ];
-
-    contextValue.orgList.map(entry  => {
-        rows.push({
-            id: entry.id,
-            name: entry.name
-        })
-        return 0;
-    });
-
-    const useStyles = makeStyles({
-        root: {
-            maxWidth: '70%',
-        },
-        container: {
-            maxHeight: 440,
-        },
-    });
-    const classes = useStyles();
-
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
-
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
 
-    return(
+    // Org Search filter field
+    const [searchString, setSearchString] = useState("");
+    const [searchColumn, setSearchColumn] = useState("name");
+    useEffect(() => {
+            let filteredResults = filterOrgs(orgList, searchColumn, searchString)
+            setOrgListFiltered(filteredResults)
+        },
+        [searchString, searchColumn, orgListFiltered])
+
+
+    return (
         <div className="orgListSection">
             <h3>LIST OF ORGS</h3>
 
@@ -78,50 +71,68 @@ const Org = () => {
                 id="orgSearch"
                 label="Org Search"
                 type="search"
-                onKeyUp={ filterOrgs }
-                variant="outlined" />
+                value={searchString}
+                onChange={e => setSearchString(e.target.value)}
+                variant="outlined"/>
 
             <div>
-                <TableContainer className={classes.container}>
-                    <Table id="myTable" stickyHeader aria-label="sticky table">
+                <TableContainer>
+                    <Table id="myTable" stickyHeader>
                         <TableHead>
                             <TableRow>
-                                {columns.map((column) => (
-                                    <TableCell
-                                        key={column.id}
-                                        align={column.align}
-                                        style={{ minWidth: column.minWidth }}
-                                    >
-                                        {column.label}
-                                    </TableCell>
-                                ))}
+                                <TableCell align="right"
+                                           style={{fontWeight: "bold", fontSize: 16}}>S.No</TableCell>
+                                <TableCell align="center" style={{fontWeight: "bold", fontSize: 16, width: 150}}>Org
+                                    Name</TableCell>
+                                <TableCell align="center" style={{fontWeight: "bold", fontSize: 16}}>Org
+                                    ID</TableCell>
+                                <TableCell align="center"
+                                           style={{fontWeight: "bold", fontSize: 16}}>URL</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                                return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                                        {columns.map((column) => {
-                                            const value = row[column.id];
-                                            return (
-                                                <TableCell key={column.id} align={column.align}>
-                                                    <Link to={`${row.id}/`}>
-                                                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                                                    </Link>
-                                                </TableCell>
-                                            );
-                                        })}
-                                    </TableRow>
-                                );
-                            })}
+                            {orgListFiltered.map((entry, index) => {
+                                if (index >= rowsPerPage * page && index < rowsPerPage * (page + 1)) {
+                                    return (
+                                        <TableRow key={entry.id}>
+                                            <TableCell align="left" style={{width: 110}}>
+                                                <Tooltip title="test">
+                                                    <InfoIcon
+                                                        fontSize="small"
+                                                    />
+                                                </Tooltip>
+                                                {(index + 1)}
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                <Link to={`${entry.id}/`}>
+                                                    {entry.name}
+                                                </Link>
+                                            </TableCell>
+                                            <TableCell align="center"
+                                                       style={{fontSize: 12}}>{entry.id}</TableCell>
+                                            <TableCell align="center">
+                                                <Link
+                                                    component="button"
+                                                    variant="body2"
+                                                    onClick={() => {
+                                                        window.open(entry.url, '_blank').focus();
+                                                    }}>
+                                                    Dashboard
+                                                </Link>
+                                            </TableCell>
+                                        </TableRow>
+                                    )
+                                }
+                            })
+                            }
                         </TableBody>
                     </Table>
                 </TableContainer>
 
                 <TablePagination
                     component="div"
-                    rowsPerPageOptions={[10, 25, 100]}
-                    count={rows.length}
+                    rowsPerPageOptions={[5, 25, 100]}
+                    count={orgListFiltered.length}
                     rowsPerPage={rowsPerPage}
                     page={page}
                     onChangePage={handleChangePage}
